00ae8278d5169840aee6b83b414a44124833640f
Kyra Mellema <kemellema@gmail.com>
2020-01-29 21:12:18 -0500

    Practice from 1/29/20

---
+++ Codecademy.js
@@ -0,0 +1,19 @@
+// Let’s create a function that prints a reminder to the console. Using a function declaration, create a function called getReminder(). In the function body of getReminder(), log the following reminder to the console: 'Water the plants.'
+function getReminder() {
+	console.log("Water the plants.");
+}
+
+// Let’s create another function that prints a useful Spanish travel phrase to the console. Using a function declaration, create a function called greetInSpanish(). In the function body console.log() the following Spanish phrase to the console: 'Buenas Tardes.'
+function greetInSpanish() {
+	console.log("Buenas Tardes.");
+}
+
+// Imagine that you manage an online store. When a customer places an order, you send them a thank you note. Let’s create a function to complete this task:
+// Define a function called sayThanks() as a function declaration. In the function body of sayThanks(), add code such that the function writes the following thank you message to the console when called: 'Thank you for your purchase! We appreciate your business.' Call sayThanks() to view the thank you message in the console. Imagine that three customers placed an order and you wanted to send each of them a thank you message. Update your code to call sayThanks() three times.
+function sayThanks() {
+	console.log("Thank you for your purchase! We appreciate your business.");
+}
+
+sayThanks();
+sayThanks();
+sayThanks();
