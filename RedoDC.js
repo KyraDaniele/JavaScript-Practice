2f8ff6d2529b28d5aa94ca8c3cb1adbef8ecd974
Kyra Mellema <kemellema@gmail.com>
2020-08-21 08:13:16 -0400

    Added more DigitalCrafts exercise answers

--- RedoDC.js
+++ RedoDC.js
@@ -14,3 +14,70 @@ function hello(name) {
 	}
 }
 hello();
+
+// 3. Write a madlib function, which is given a name and a subject. It will return(not print) a new string: (name)'s favorite subject in school is (subject).
+function madlib(name, subject) {
+	return name + "'s favorite subject is " + subject;
+}
+console.log(madlib("Kyra", "coding"));
+
+// 4. Write a function tipAmount that is given the bill amount and the level of service (one of good, fair and poor) and returns the dollar amount for the tip. Based on:
+// good -> 20%
+// fair -> 15%
+// bad -> 10%
+let tip = 0;
+function tipAmount(bill, service) {
+	if (service === "good") {
+		return "Your tip is $" + (tip = bill * 0.2);
+	} else if (service === "fair") {
+		return "Your tip is $" + (tip = bill * 0.15);
+	} else if (service === "bad") {
+		return "Your tip is $" + (tip = bill * 0.1);
+	} else {
+		return "Please say if your service was good, fair, or bad.";
+	}
+}
+console.log(tipAmount(100, "good"));
+console.log(tipAmount(57, "fair"));
+console.log(tipAmount(29, "bad"));
+console.log(tipAmount(92, "decent"));
+
+// 5. Write a function totalAmount that takes the same arguments as tipAmount except it returns the total as the tip amount plus the bill amount. This function may make use of tipAmount as a sub-task.
+let tip = 0;
+let finalBill = 0;
+function totalAmount(bill, service) {
+	if (service === "good") {
+		tip = bill * 0.2;
+	} else if (service === "fair") {
+		tip = bill * 0.15;
+	} else if (service === "bad") {
+		tip = bill * 0.1;
+	} else {
+		return "Please say if your service was good, fair, or bad.";
+	}
+	return "Your total with tip is $" + (finalBill = tip + bill);
+}
+console.log(totalAmount(100, "good"));
+console.log(totalAmount(57, "fair"));
+console.log(totalAmount(29, "bad"));
+console.log(totalAmount(92, "decent"));
+
+// 6.
+let tip = 0;
+let finalBill = 0;
+function totalAmount(bill, service, people) {
+	if (service === "good") {
+		tip = bill * 0.2;
+	} else if (service === "fair") {
+		tip = bill * 0.15;
+	} else if (service === "bad") {
+		tip = bill * 0.1;
+	} else {
+		return "Please say if your service was good, fair, or bad.";
+	}
+	return "Each person pays $" + (finalBill = (tip + bill) / people);
+}
+console.log(totalAmount(100, "good", 5));
+console.log(totalAmount(57, "fair", 3));
+console.log(totalAmount(29, "bad", 2));
+console.log(totalAmount(92, "decent", 7));
